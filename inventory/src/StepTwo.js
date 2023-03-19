e8a934aee641203097273d4f6f273c7aec4b334f
JohnWick08 <mgao041@uottawa.ca>
2023-03-18 21:35:40 -0400

    update

--- inventory/src/StepTwo.js
+++ inventory/src/StepTwo.js
@@ -6,7 +6,7 @@ const StepOne = (props) => {
 
     return (
         <div>
-                <Header />
+            <Header />
 
             <div className="wrapper">
                 <div className='sub-header'>
