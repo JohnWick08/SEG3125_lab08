d497ce301d1af31c0a64a3c691956b9ae09d2687
Vicky-Feng <71288189+Vicky-Feng@users.noreply.github.com>
2023-03-23 21:58:11 -0400

    implemented step2

--- inventory/src/StepTwo.js
+++ inventory/src/StepTwo.js
@@ -3,11 +3,36 @@ import Header from './Header';
 import './App.css';
 const StepTwo = (props) => {
 
+    const score = parseInt(localStorage.getItem('myScore'));
+    let imageSource;
+    let recommandation;
+    let level;
+
+    if (score >= 0 && score < 6){
+        imageSource = '../images/level1.jpg';
+        level= "1 No Depress"
+        recommandation="Well done! Keep going!"
+    } else if (score >= 6 && score < 11) {
+        imageSource = '../images/level2.jpg';
+        level= "2 Moderate Depress"
+        recommandation="Take it easy! You can do some excise to relax."
+    } else if (score >= 11 && score < 16) {
+        imageSource = '../images/level3.jpg';
+        level= "3 Moderate Depress"
+        recommandation="Take a deep breath. Please remember that you’re capable, brave and loved – even when it feels like you’re not. "
+    } else if (score >= 16 && score < 21) {
+        imageSource = '../images/level4.jpg';
+        level= "4 Extreme Depress"
+        recommandation="You can try meditation to help you relax. Just a reminder: it is NOT selfish to put your recovery first. Rather, it’s necessary in order to make sure that everything else doesn’t come last."
+    } else {
+        imageSource = '../images/level5.jpg';
+        level= "5 Extreme Depress"
+        recommandation="Love yourself! Even the darkest hour only has 60 minutes."
+    }
 
     return (
         <div>
             <Header />
-
             <div className="wrapper">
                 <div className='sub-header'>
                     <div className="subheader-content">
@@ -15,20 +40,23 @@ const StepTwo = (props) => {
                     </div>
                 </div>
             </div>
-            <div className='pic' margin='auto'>
-                <div className='realpic' float='left'>
-                    <img src='./images/level1.jpg' width='50%'></img>
+            <div class="box-container-numbered"></div>
+
+                <div className='numbered-subheading' float='left'>
+                    <p>According to Your input, your level of depression is: <b>{level}</b></p>
                 </div>
-                <div className='intropic' float='left'>
-                    <p>According to Your input,your level of depression is: <b>5 Extreme Depress</b></p>
-                </div>
-                <div className='recomm' float='left'>
-                    <p><b>Recommendation:</b><br></br>
-                       Drink hot water!</p>
-                </div>
+            <br></br>
+
+            <div className='realpic' >
+                <img className="result" src={imageSource} alt={`Image for score ${score}`}/>
             </div>
-            <p>{localStorage.getItem('myScore')}</p>
+            <br></br> <br></br> <br></br>
+            <div class='center'>
+                <p class="centerp">Recommendation:</p>
+                   <p class="recommandation"> {recommandation}</p>
+            </div>
         </div>
+
     );
 }
 
