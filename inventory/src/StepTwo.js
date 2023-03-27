2a1677cc25b2927f02d1b9677643a1c1e8c361a9
Vicky-Feng <71288189+Vicky-Feng@users.noreply.github.com>
2023-03-26 21:34:35 -0400

    update french version

--- inventory/src/StepTwo.js
+++ inventory/src/StepTwo.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import Header from './Header';
 import './App.css';
+
+
 const StepTwo = (props) => {
     let lang = localStorage.getItem("language");
     const score = parseInt(localStorage.getItem('myScore'));
@@ -32,23 +34,23 @@ const StepTwo = (props) => {
         }
     }else{
         if (score >= 0 && score < 6){
-            imageSource = '../images/level1.jpg';
+            imageSource = '../images/level1_fr.jpg';
             level= "1 Pas d'enfoncement"
             recommandation="Bien joué! Continuer!"
         } else if (score >= 6 && score < 11) {
-            imageSource = '../images/level2.jpg';
+            imageSource = '../images/level2_fr.jpg';
             level= "2 Dépression modérée"
             recommandation="Allez-y doucement! Vous pouvez faire quelques excises pour vous détendre."
         } else if (score >= 11 && score < 16) {
-            imageSource = '../images/level3.jpg';
+            imageSource = '../images/level3_fr.jpg';
             level= "3 Dépression modérée"
             recommandation="Respirez profondément. N'oubliez pas que vous êtes capable, courageux et aimé - même quand vous avez l'impression que vous ne l'êtes pas."
         } else if (score >= 16 && score < 21) {
-            imageSource = '../images/level4.jpg';
+            imageSource = '../images/level4_fr.jpg';
             level= "4 Dépression extrême"
             recommandation="Vous pouvez essayer la méditation pour vous aider à vous détendre. Juste un rappel : ce n'est PAS égoïste de mettre votre rétablissement en premier. Au contraire, c'est nécessaire pour s'assurer que tout le reste ne vient pas en dernier."
         } else {
-            imageSource = '../images/level5.jpg';
+            imageSource = '../images/level5_fr.jpg';
             level = "5 Dépression extrême"
             recommandation = "Aime toi toi-même! Même l'heure la plus sombre n'a que 60 minutes."
         }
